a62d5f22d8feb785dd1ee47e3255833d3bd4f3f8
Adriano Martinelli <adrianom@student.ethz.ch>
2025-05-22 23:22:49 +0200

    prepare for relaese

--- src/ai4bmr_datasets/datasets/__init__.py
+++ src/ai4bmr_datasets/datasets/__init__.py
@@ -1,6 +1,9 @@
-from .DummyTabular import DummyTabular
-from .DummyImages import DummyImages
 
 from .TNBC import TNBC
 from .Cords2024 import Cords2024
-from .PCa import PCa
+from .Danenberg2022 import Danenberg2022
+from .Jackson2023 import Jackson2023
+
+from .DummyPoints import DummyPoints
+from .DummyImages import DummyImages
+from .DummyTabular import DummyTabular
