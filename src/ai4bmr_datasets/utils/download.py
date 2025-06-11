9e97793a9eb46570041708f6cd93d60c4b0425a8
Adriano Martinelli <adrianom@student.ethz.ch>
2025-06-11 10:04:24 +0200

    skip root

--- src/ai4bmr_datasets/utils/download.py
+++ src/ai4bmr_datasets/utils/download.py
@@ -9,12 +9,16 @@ def unzip_recursive(zip_path: Path, extract_dir: Path = None):
         extract_dir = extract_dir or zip_path.parent / zip_path.stem
         is_extracted = all((extract_dir / name).exists() for name in extracted_names)
         if is_extracted:
-            if logger:
-                logger.info(f"Skipping extraction for {zip_path.name}, files already exist.")
+            logger.info(f"Skipping extraction for {zip_path.name}, files already exist.")
         else:
-            if logger:
-                logger.info(f"Unzipping {zip_path.name}")
-            zip_ref.extractall(extract_dir)
+            logger.info(f"Unzipping {zip_path.name}")
+
+            # zip_ref.extractall(extract_dir)
+            for zip_info in zip_ref.infolist():
+                if zip_info.filename == '/':
+                    continue
+                elif zip_info.filename.strip():  # Skip empty or whitespace-only filenames
+                    zip_ref.extract(zip_info, extract_dir)
 
         # Recursively unzip any .zip files in extracted content
         for name in extracted_names:
