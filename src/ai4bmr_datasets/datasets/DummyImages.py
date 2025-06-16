bfe5bc611796ea6a6684403e1747d7f1dd4910ed
Adriano Martinelli <adrianom@student.ethz.ch>
2025-06-16 19:32:39 +0200

    tidy dummy images

--- src/ai4bmr_datasets/datasets/DummyImages.py
+++ src/ai4bmr_datasets/datasets/DummyImages.py
@@ -1,54 +1,8 @@
 import numpy as np
 import pandas as pd
-
-
-class DummyTabular:
-
-    def __init__(
-        self, num_samples: int = 1000, num_features: int = 10, num_classes: int = 2
-    ):
-        self.num_samples = num_samples
-        self.num_features = num_features
-
-        rng = np.random.default_rng(seed=42)
-
-        self.data = pd.DataFrame(rng.random((self.num_samples, self.num_features)))
-        self.data.index = self.data.index.astype(str)
-        self.data.index.name = "sample_id"
-
-        metadata = pd.DataFrame(
-            rng.integers(0, num_classes, num_samples),
-            columns=["label_id"],
-            dtype="category",
-        )
-
-        self.metadata = metadata.convert_dtypes()
-        self.metadata["label"] = [
-            ["type_1", "type_2"][i] for i in self.metadata["label_id"]
-        ]
-        self.metadata["label"] = self.metadata["label"].astype("category")
-        self.metadata = self.metadata.convert_dtypes()
-        self.metadata.index = self.metadata.index.astype(str)
-        self.metadata.index.name = "sample_id"
-
-        self.sample_ids = self.metadata.index.to_list()
-
-    def load(self):
-        return dict(data=self.data, metadata=self.metadata)
-
-    def __getitem__(self, idx):
-        sample_id = self.sample_ids[idx]
-        return {
-            "sample_id": sample_id,
-            "data": self.data.loc[sample_id].to_numpy(),
-            "metadata": self.metadata.loc[sample_id].to_dict(),
-        }
-
-
 from pathlib import Path
 from skimage.io import imsave
 from ..datamodels.Image import Image
-
 
 class DummyImages:
 
@@ -61,6 +15,8 @@ class DummyImages:
         width: int = 32,
         num_classes: int = 2,
     ):
+        self.image = self.metadata = None
+
         self.save_dir = Path(save_dir).expanduser().resolve()
         self.images_dir = self.save_dir / "images"
         self.images_dir.mkdir(exist_ok=True, parents=True)
@@ -68,7 +24,7 @@ class DummyImages:
         self.metadata_dir = self.save_dir / "metadata"
         self.metadata_dir.mkdir(exist_ok=True, parents=True)
 
-        self.num_sample = num_samples
+        self.num_samples = num_samples
         self.num_channels = num_channels
         self.height = height
         self.width = width
@@ -76,13 +32,13 @@ class DummyImages:
 
         rng = np.random.default_rng(seed=42)
 
-        targets = rng.integers(0, num_classes, self.num_sample)
+        targets = rng.integers(0, num_classes, self.num_samples)
         for i, label in enumerate(targets):
-            image = rng.random((self.num_channels, self.height, self.width)).astype(
-                np.float16
-            )
+
+            image = rng.random((self.num_channels, self.height, self.width)).astype(np.float32)
             imsave(self.images_dir / f"{i}.tiff", image)
-            metadata = pd.DataFrame({"label_id": label}, index=[i])
+
+            metadata = pd.DataFrame({"target": label}, index=[i])
             metadata.index.name = "sample_id"
             metadata.to_parquet(self.metadata_dir / f"{i}.parquet")
 
@@ -90,9 +46,29 @@ class DummyImages:
         panel.to_parquet(self.panel_path)
 
     def setup(self):
-        images = [
+        self.images = [
             Image(data_path=p, metadata_path=self.panel_path)
             for p in self.images_dir.glob("*.tiff")
         ]
-        metadata = pd.read_parquet(self.metadata_dir)
-        return dict(images=images, metadata=metadata)
+        self.metadata = pd.read_parquet(self.metadata_dir)
+
+from torch.utils.data import Dataset
+class DummyImagesTorch(Dataset):
+    def __init__(self, num_samples=1000, num_classes=2, num_channels=3, height=224, width=224):
+        from ai4bmr_datasets.datasets.DummyImages import DummyImages
+        self.dataset = DummyImages(num_samples=num_samples, num_classes=num_classes, num_channels=num_channels,
+                                   height=height, width=width)
+
+    def setup(self):
+        self.dataset.setup()
+
+    def __len__(self):
+        return self.dataset.num_samples
+
+    def __getitem__(self, idx):
+        image = self.dataset.images[idx].data
+        target = self.dataset.metadata.loc[idx, 'target']
+
+        image = torch.from_numpy(image).float()
+        target = torch.tensor(target).long()
+        return {'image': image.data, 'target': target}
--- src/ai4bmr_datasets/datasets/__init__.py
+++ src/ai4bmr_datasets/datasets/__init__.py
@@ -1,9 +1,11 @@
-
 from .Keren2018 import Keren2018
 from .Cords2024 import Cords2024
 from .Danenberg import Danenberg2022
 from .Jackson2020 import Jackson2020
+
 from .PCa import PCa
+
+from .BEAT import BEAT
 
 from .DummyPoints import DummyPoints
 from .DummyImages import DummyImages
