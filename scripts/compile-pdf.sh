3c6537587e10db9e11e3870fb8e3cc6ba2944216
adrianomartinelli <adrianom@student.ethz.ch>
2025-06-16 14:48:35 +0200

    prepare arxiv version

--- scripts/compile-pdf.sh
+++ scripts/compile-pdf.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 pandoc paper.md --citeproc --bibliography=paper.bib -s -o paper.pdf -V geometry:margin=1.5in
+pandoc paper-arxiv.md -citeproc --bibliography=paper.bib -s -o paper-arxiv.pdf --template=default.tex -V geometry:margin=1.5in
+#pandoc paper-arxiv.md --bibliography=paper.bib -s -o paper-arxiv.tex --template=default.tex -V geometry:margin=1.5in --citeproc
